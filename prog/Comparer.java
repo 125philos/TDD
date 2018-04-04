e3163fd84f6e51ef4c4798a680c2d1004c0604b8
Olessya <philos.dec@gmail.com>
2018-04-04 13:21:36 +0700

    Add comments to method "compareSymbolArrays".

--- prog/Comparer.java
+++ prog/Comparer.java
@@ -5,11 +5,17 @@ public class Comparer {
 	static boolean arrayCongruence = false; // flag congruence 
 	
 	// compare two array
+	/*
+	 * Метод, который направлен на проверяем конгруэнтность массивов.  
+	 * @param arr1 - массив с первым комплектом символов, типа char, 
+	 * @param arr2 - массив со вторым комплектом символов, типа char.
+	 * @return arrayCongruence - результат сравнения двух массивов, тип boolean.
+	 * */
 	public static boolean compareSymbolArrays(char[] arr1, char[] arr2) {
 		arrayCongruence = true;
         for (int i = 0; i < arr1.length; i++) {
             if (arr1[i] != arr2[i]) {
-				arrayCongruence= false;
+				arrayCongruence = false;
             }
         }
         return arrayCongruence;
