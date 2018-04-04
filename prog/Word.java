42359d99a9fad8628bed7da0232795a5cd5bf1e0
Olessya <philos.dec@gmail.com>
2018-04-04 11:38:40 +0700

    Добавлен класс Word.java, конструктор без параметров и переменные для проверки работоспособности теста.

---
+++ prog/Word.java
@@ -0,0 +1,12 @@
+package prog;
+
+public class Word {
+
+	public static final char NULL_SYMBOL = '\u0000'; // Нулевой символ
+	private char elmentToEncrypt; // Элемент для шифрования
+	
+	//constructor
+	public Word() {
+        this.elmentToEncrypt = NULL_SYMBOL;
+    }
+}
