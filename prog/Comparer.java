ab7cd4695d001ea36ff6c3056b64c63a6c336de0
Olessya <philos.dec@gmail.com>
2018-04-04 13:12:53 +0700

    Добавили метод "compareSymbolArrays", и тест "congruenceTwoArrayTest" - прошел успешно.

--- prog/Comparer.java
+++ prog/Comparer.java
@@ -2,5 +2,16 @@ package prog;
 
 public class Comparer {
 
+	static boolean arrayCongruence = false; // flag congruence 
 	
+	// compare two array
+	public static boolean compareSymbolArrays(char[] arr1, char[] arr2) {
+		arrayCongruence = true;
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+				arrayCongruence= false;
+            }
+        }
+        return arrayCongruence;
+    }
 }
